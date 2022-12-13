78f460f92b16f6df8ee07f2546cd66cc00bb115f
kzoriz <borisoliveira@alu.uern.br>
2022-12-12 22:58:08 -0300

    sobreNos db

--- cosmos_solar/views.py
+++ cosmos_solar/views.py
@@ -5,9 +5,21 @@ from cosmos_solar.models import SobreNos
 
 # Create your views here.
 def home(request, pk=None):
-    text = SobreNos.objects.get(pk=1)
-    context = {
-        "object": text,
+    sobre_nos = SobreNos.objects.all()
 
-    }
-    return render(request, "cosmos_solar/index.html", context)
+    if len(sobre_nos) == 0:
+        context2 = {
+            "object.title1": "empty",
+            "object.title2": "empty",
+            "object.text": "empty",
+
+        }
+        return render(request, "cosmos_solar/index.html", context2)
+    else:
+        text = SobreNos.objects.first()
+        context = {
+            "object": text,
+
+        }
+        return render(request, "cosmos_solar/index.html", context)
+
