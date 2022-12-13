ccfe95ac4c7396a8f8f2ec8993ac6627c6db95a5
kzoriz <borisoliveira@alu.uern.br>
2022-12-12 22:06:51 -0300

    os.enviro.get

--- solar/settings.py
+++ solar/settings.py
@@ -21,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3r4t7k$!fp3!hi4tr*5dw5fv()8!+qibgl3a!57d#@wwoy5&sc'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
